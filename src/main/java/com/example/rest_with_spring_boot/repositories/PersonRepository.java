83aee9cb7222605fbfaa12169970acfc60b39948
Bruno Vieira <brunovws77@gmail.com>
2024-08-23 18:24:00 -0300

    VO standard applied.

--- src/main/java/com/example/rest_with_spring_boot/repositories/PersonRepository.java
+++ src/main/java/com/example/rest_with_spring_boot/repositories/PersonRepository.java
@@ -1,9 +1,7 @@
 package com.example.rest_with_spring_boot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.rest_with_spring_boot.model.Person;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long>{}
--- src/test/java/com/example/rest_with_spring_boot/Startup.java
+++ src/test/java/com/example/rest_with_spring_boot/Startup.java
@@ -7,7 +7,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 class Startup {
 
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
