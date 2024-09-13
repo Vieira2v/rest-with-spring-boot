acb51917d3395c316f200427d1e019b329775614
Bruno Vieira <brunovws77@gmail.com>
2024-09-13 18:02:06 -0300

    Upload files.

---
+++ src/main/java/com/example/rest_with_spring_boot/data_vo_v1/UploadFileResponseVO.java
@@ -0,0 +1,60 @@
+package com.example.rest_with_spring_boot.data_vo_v1;
+
+import java.io.Serializable;
+
+public class UploadFileResponseVO implements Serializable {
+    
+    private static final long SERIAL_VERSION_ID = 1L;
+
+    private String fileName;
+    private String fileDowloadUri;
+    private String fileType;
+    private long size;
+    
+    public UploadFileResponseVO(){}
+
+    public UploadFileResponseVO(String fileName, String fileDowloadUri, String fileType, long size) {
+        this.fileName = fileName;
+        this.fileDowloadUri = fileDowloadUri;
+        this.fileType = fileType;
+        this.size = size;
+    }
+
+    public static long getSerialVersionId() {
+        return SERIAL_VERSION_ID;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileDowloadUri() {
+        return fileDowloadUri;
+    }
+
+    public void setFileDowloadUri(String fileDowloadUri) {
+        this.fileDowloadUri = fileDowloadUri;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    
+}
---
+++ src/main/java/com/example/rest_with_spring_boot/services/FileStorageService.java
@@ -0,0 +1,7 @@
+package com.example.rest_with_spring_boot.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileStorageService {
+}
